f377c5af43cafe6663839164635d97d1acf7f044
Wynn <1052209675@qq.com>
2023-02-17 09:40:05 +0800

    wrong
    Signed-off-by: Wynn <1052209675@qq.com>

--- inference.py
+++ inference.py
@@ -52,43 +52,44 @@ def homography_mapping(features, extrinsics, intrinsics, depth_values, nviews):
         relative_rotation = torch.matmul(torch.inverse(src_rotation), ref_rotation)
         relative_trans = ref_trans - src_trans
         normal_T = torch.transpose(torch.Tensor([[0, 0, 1]]), 0, 1).unsqueeze(0).to(ref_feature.device)
-        print("relative_rotation.shape: {}".format(relative_rotation.shape))
-        print("relative_rotation.device: {}".format(relative_rotation.device))
-        print("normal_T.device: {}".format(normal_T.device))
-        print("volume.device: {}".format(volume.device))
-        
-        i = 0
-        for depth in depth_values[0]:
-            Homo = torch.matmul(torch.matmul(src_intrinsic, relative_rotation + torch.transpose(relative_rotation, 1, 2) * relative_trans * normal_T / depth), torch.inverse(ref_intrinsic))
-            print("Homo", Homo.shape) # the shape of Homo should be [3, 3], freedom of 8
-            y, x = torch.meshgrid([torch.arange(0, H, dtype=torch.float32, device=ref_feature.device),
-                                    torch.arange(0, W, dtype=torch.float32, device=ref_feature.device)])
-            y, x = y.contiguous(), x.contiguous()
-            y, x = y.view(H * W), x.view(H * W)
-            xyz = torch.stack((x, y, torch.ones_like(x))) # torch.Size([3, 327680])
-            proj_xyz = torch.matmul(torch.inverse(Homo), xyz) # [batch_size, 3, H*W]
-            proj_xy = proj_xyz[:, :2] / proj_xyz[:, 2]
-            proj_x_normalized = proj_xy[:, 0, :] / ((W - 1) / 2) - 1 # [batch_size, H*W]
-            proj_y_normalized = proj_xy[:, 1, :] / ((H - 1) / 2) - 1 # [batch_size, H*W]
-            grid = torch.stack((proj_x_normalized, proj_y_normalized), dim=2) # [batch_size, H*W, 2]
-            grid = grid.view(batch_size, H, W, 2)
-            output = F.grid_sample(src_fea, grid)
-            print(output.shape)
-            print(volume.shape)
-            volume[:, i] = output
-            i += 1
-        volume_sum += volume
-        volume_square += volume.pow(2)
+        # print("relative_rotation.shape: {}".format(relative_rotation.shape))
+        # print("relative_rotation.device: {}".format(relative_rotation.device))
+        # print("normal_T.device: {}".format(normal_T.device))
+        # print("volume.device: {}".format(volume.device))
+        with torch.no_grad(): # TODO: I am not sure about this
+            i = 0
+            for depth in depth_values[0]:
+                Homo = torch.matmul(torch.matmul(src_intrinsic, relative_rotation + torch.transpose(relative_rotation, 1, 2) * relative_trans * normal_T / depth), torch.inverse(ref_intrinsic))
+                # print("Homo", Homo.shape) # the shape of Homo should be [3, 3], freedom of 8
+                y, x = torch.meshgrid([torch.arange(0, H, dtype=torch.float32, device=ref_feature.device),
+                                        torch.arange(0, W, dtype=torch.float32, device=ref_feature.device)])
+                y, x = y.contiguous(), x.contiguous()
+                y, x = y.view(H * W), x.view(H * W)
+                xyz = torch.stack((x, y, torch.ones_like(x))) # torch.Size([3, 327680])
+                proj_xyz = torch.matmul(torch.inverse(Homo), xyz) # [batch_size, 3, H*W]
+                proj_xy = proj_xyz[:, :2] / proj_xyz[:, 2]
+                proj_x_normalized = proj_xy[:, 0, :] / ((W - 1) / 2) - 1 # [batch_size, H*W]
+                proj_y_normalized = proj_xy[:, 1, :] / ((H - 1) / 2) - 1 # [batch_size, H*W]
+                grid = torch.stack((proj_x_normalized, proj_y_normalized), dim=2) # [batch_size, H*W, 2]
+                grid = grid.view(batch_size, H, W, 2)
+                output = F.grid_sample(src_fea, grid)
+                # print(output.shape)
+                # print(volume.shape)
+                volume[:, i] = output
+                i = i + 1
+        volume_sum = volume_sum + volume
+        volume_square = volume_square + volume.pow(2)
     cost_volume = volume_square / nviews - (volume_sum / nviews).pow(2)
     return cost_volume
 
 # probility_volume: [batch_size, D, H, W]
 # depth_values: [batch_size, D]
 def depth_regression(probility_volume, depth_values):
-    print(probility_volume.shape)
-    print(depth_values.shape)
-    print(probility_volume.device)
-    print(depth_values.device)
+    # print(probility_volume.shape)
+    # print(depth_values.shape)
+    # print(probility_volume.device)
+    # print(depth_values.device)
+    # TODO: I am not sure about this
     d = depth_values.view(probility_volume.shape[0], probility_volume.shape[1], 1, 1)
     depth = torch.sum(d * probility_volume, 1)
     return depth
@@ -140,8 +141,8 @@ class MVSVolume(pl.LightningModule):
         cost_volume = homography_mapping(features, extrinsics, intrinsics, depth_values, nviews)
         probility_volume = F.softmax(self.VolumeReg(cost_volume.transpose(1,2)).squeeze(1), dim=1)
         depth_map = depth_regression(probility_volume, depth_values).transpose(1, 2)
-        print(depth_map.shape)
-        print(depth_gt.shape)
+        # print(depth_map.shape)
+        # print(depth_gt.shape)
         loss = F.mse_loss(depth_map, depth_gt)
         return loss
     def configure_optimizers(self):
@@ -166,6 +167,7 @@ model.to(device)
  
 # Train the model
 if __name__ == '__main__':
+    torch.autograd.set_detect_anomaly = True
     # imgs, intrinsics, extrinsics, ref_depth = dataset[0]
     trainer = pl.Trainer(limit_train_batches=100, max_epochs=1, gpus=1)
     trainer.fit(model, train_dataloader=train_loader)
